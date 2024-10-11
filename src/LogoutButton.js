b663dc827dfb8953647a44d5e68049b4675df02a
suddenly-inui <inuiyuki0904@gmail.com>
2024-10-11 13:23:28 +0900

    logout button

---
+++ src/LogoutButton.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './getToken.js';
+import { accessUrl } from "./getToken.js";
+
+
+function LogoutButton(props) {
+
+    const [token, setToken] = useState(localStorage.getItem("spotifyToken"))
+
+    const handleLogout = () => {
+        if (props.isLogin) {
+            localStorage.removeItem('spotifyToken'); // ログアウト時にトークンを削除
+            setToken(localStorage.getItem("spotifyToken"))
+
+            window.location.reload(); // ページをリロードしてログイン画面に戻る
+            
+            props.setLoginState(false) //ログイン状態ならログアウト状態にする
+        }
+    };
+
+    return (
+        <div className="App">
+            <button onClick={handleLogout}>ログアウト</button>
+        </div>
+    );
+}
+
+export default LogoutButton;
+
