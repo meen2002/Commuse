a4f4457ae83a74922f7f2a7889187f400ccf5c49
meen2002 <meen2002@icloud.com>
2024-10-19 00:35:37 +0900

    Add files via upload

---
+++ src/LogoutButton.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+function LogoutButton(props) {
+
+    const [token, setToken] = useState(localStorage.getItem("spotifyToken"))
+
+    
+
+    const handleLogout = () => {
+        if (props.isLogin) {
+            localStorage.removeItem('spotifyToken'); // ログアウト時にトークンを削除
+            setToken(localStorage.getItem("spotifyToken"))
+
+            window.location.reload(); // ページをリロードしてログイン画面に戻る
+
+            props.setIsLogin(false) //ログイン状態ならログアウト状態にする
+        }
+    };
+
+
+
+    return (
+        <div className="App">
+            <button onClick={handleLogout}>ログアウト</button>
+        </div>
+
+        
+    );
+}
+
+export default LogoutButton;
+
