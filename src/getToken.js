91215994d3bdc9c8a6dd1fa85f183a25014af8df
meen2002 <meen2002@icloud.com>
2025-01-04 14:29:19 +0900

    Update getToken.js

--- src/getToken.js
+++ src/getToken.js
@@ -2,7 +2,7 @@
 export const authConfig = {
   endpoint: "https://accounts.spotify.com/authorize",
   clientId: "e631bbfc47864268998147bf7d9ee775",
-  redirectUri: "https://commuse.vercel.app/",
+  redirectUri: "https://commuse.vercel.app" || "http://localhost:3000/",,
   scopes: [
     "user-read-currently-playing",
     "user-read-playback-state",
